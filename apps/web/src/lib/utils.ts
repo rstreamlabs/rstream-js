372fc30725f252a4646a95e8a194086a5d69c089
uartnet <140632163+uartnet@users.noreply.github.com>
2025-09-17 16:57:10 +0200

    add webtty demo page

---
+++ apps/web/src/lib/utils.ts
@@ -0,0 +1,8 @@
+// See LICENSE file in the project root for license information.
+
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
